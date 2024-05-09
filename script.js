d8fa5ed3ccef9310f5cd7286037fedf6455e912d
Kefilwe Magasa <kefi.magasa@gmail.com>
2024-05-08 21:40:17 -0400

    loop

---
+++ script.js
@@ -0,0 +1,10 @@
+// Part 1
+
+const adventurer = {
+    name: "Robin",
+    health: 10,
+    inventory: ["sword", "potion", "artifact"]
+    }
+    for (let i = 0; i < adventurer.inventory.length; i++) {
+        console.log(adventurer.inventory[i]);
+      }
