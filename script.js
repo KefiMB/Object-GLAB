495df21e388fee2db366f66369349e9f941ca5e6
Kefilwe Magasa <kefi.magasa@gmail.com>
2024-05-08 21:51:56 -0400

    sub-object

--- script.js
+++ script.js
@@ -3,8 +3,24 @@
 const adventurer = {
     name: "Robin",
     health: 10,
-    inventory: ["sword", "potion", "artifact"]
+    inventory: ["sword", "potion", "artifact"],
+    companion: {
+        name: "Leo",
+        type: "Cat"
     }
+}
+const leo = {
+    name: "Leo",
+    type: "Cat",
+    friend: adventurer,
+    companion: {
+      name: "Frank",
+      type: "Flea",
+      belongings: ["small hat", "sunglasses"]
+    }
+}
     for (let i = 0; i < adventurer.inventory.length; i++) {
         console.log(adventurer.inventory[i]);
       }
+      console.log(adventurer);
+      
